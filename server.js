a0520c4965e95a0ea8fe6e310bfe1f7117c69891
Mohammad Hezam <hezammohammed416@gmail.com>
2025-09-27 00:46:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,4 @@
-{
-  "version": 2,
-  "builds": [
-    { "src": "backend/server.js", "use": "@vercel/node" },
-    { "src": "frontend/index.html", "use": "@vercel/static" }
-  ],
-  "routes": [
-    { "src": "/api/(.*)", "dest": "backend/server.js" },
-    { "src": "/(.*)", "dest": "frontend/index.html" }
-  ]
-}
+mongoose.connect('MONGODB_URI="mongodb+srv://Vercel-Admin-atlas-mohammed-hezam:JGzF9CVouYbH4Ksy@atlas-mohammed-hezam.3lldjh8.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(()=> console.log("MongoDB connected"));
